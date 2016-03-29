57927b927b88d7507e669a17443d26929f79b360
MinervaP <GoddessMinervaP@gmail.com>
2016-03-30 02:31:01 +0900

    add .vimperatorrc

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.pryrc ~/.pryrc
+ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
