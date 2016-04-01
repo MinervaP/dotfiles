415e57fde5bcf568b60b43ea02a77e1145908884
MinervaP <GoddessMinervaP@gmail.com>
2016-04-02 04:35:56 +0900

    あれ

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -2,6 +2,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.pryrc ~/.pryrc
 ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
