3de432ba05356d516ae231d64db5f7a78fe77027
minerva1129 <goddessminervap@gmail.com>
2016-05-16 08:52:17 +0900

    Install Homebrew

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+if [ ! $(uname -s) == "Darwin" ]; then
+
+  echo "The install script can run only on OS X"
+  exit 1
+
+else
+
+echo "Linking dotfiles ..."
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
@@ -6,3 +16,15 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.pryrc ~/.pryrc
 ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
+
+if [ ! `which brew` ]; then
+  echo "Installing Homebrew ..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Using Homebrew"
+
+brew tap homebrew/brewdler
+brew bundle
+
+fi
